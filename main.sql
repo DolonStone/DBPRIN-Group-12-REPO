41866257d0f0a012a522dbbaaf58951321395d97
Joshua Rymer <joshrymer0@gmail.com>
2025-12-11 13:58:42 +0000

    recommit

--- main.sql
+++ main.sql
@@ -1,5 +1,5 @@
 CREATE TABLE service_task(
-    service_task_id SERAIAL PRIMARY KEY,
+    service_task_id SERIAL PRIMARY KEY,
     booking_id SMALLINT NOT NULL,
     service_id SMALLINT NOT NULL,
     service_task_status ENUM('Pending', 'In Progress', 'Completed') DEFAULT 'Pending',
@@ -9,47 +9,53 @@ CREATE TABLE service_task(
     FOREIGN KEY (booking_id) REFERENCES booking(booking_id),
     FOREIGN KEY (service_id) REFERENCES service_detail(service_id),
     FOREIGN KEY (bay_id) REFERENCES bay(bay_id)
-) CREATE TABLE bay(
+)
+CREATE TABLE bay(
     bay_id SERIAL PRIMARY KEY,
     bay_last_inspection_date SMALLDATETIME,
     bay_status ENUM('Available', 'Occupied', 'Under Maintenance') DEFAULT 'Available' bay_inspection_result ENUM('Pass', 'Fail')
-) CREATE TABLE staff_allocation(
+) 
+CREATE TABLE staff_allocation(
     service_task_id SMALLINT NOT NULL,
     staff_id SMALLINT NOT NULL,
     allocation_role ENUM('Technician', 'Supervisor', 'Assistant') NOT NULL,
     PRIMARY KEY (service_task_id, staff_id),
     FOREIGN KEY (service_task_id) REFERENCES service_task(service_task_id),
     FOREIGN KEY (staff_id) REFERENCES staff(staff_id)
-) Create TABLE service_detail(
+) 
+Create TABLE service_detail(
     service_id SERIAL PRIMARY KEY,
     service_name VARCHAR(100) NOT NULL,
     service_description TEXT,
     service_duration SMALLINT NOT NULL,
     service_cost DECIMAL(10, 2) NOT NULL
-) CREATE TABLE car_parts(
+) 
+CREATE TABLE car_parts(
     service_id SMALLINT NOT NULL,
     part_id SMALLINT NOT NULL,
     part_amount SMALLINT NOT NULL,
     PRIMARY KEY (service_id, part_id),
     FOREIGN KEY (service_id) REFERENCES service_detail(service_id),
     FOREIGN KEY (part_id) REFERENCES parts_inventory(part_id)
-) CREATE TABLE parts_inventory(
+) 
+CREATE TABLE parts_inventory(
     part_id SERIAL PRIMARY KEY,
     part_name VARCHAR(100) NOT NULL,
     part_supplier_id SMALLINT NOT NULL,
     FOREIGN KEY (part_supplier_id) REFERENCES supplier(supplier_id)
-) CREATE TABLE supplier(
+) 
+CREATE TABLE supplier(
     supplier_id SERIAL PRIMARY KEY,
     supplier_name VARCHAR(100) NOT NULL,
     supplier_contact VARCHAR(100),
     supplier_address TEXT
-)
+);
 
 
 CREATE TABLE booking (
     booking_ID SERIAL PRIMARY KEY,
-    booking_date SMALLDATETIME,
-    Scheduled_Date SMALLDATETIME,
+    booking_date DATETIME,
+    Scheduled_Date DATETIME,
     Scheduled_Time SMALLINT,
     Status ENUM('Pending', 'Confirmed', 'Cancelled', 'Completed'),
     Customer_ID SMALLINT,
